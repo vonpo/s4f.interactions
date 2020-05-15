ba2b919d9ccf8c7e99cf2f700e07613316e6556e
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2020-05-15 14:47:56 +0200

    Remove redirection for app.js

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,5 +1,6 @@
 exports.config = {
-	//seleniumAddress: 'http://localhost:4444/wd/hub',
+	//seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+	seleniumAddress: 'http://192.168.56.1:4444/wd/hub',
 	specs: ['specs/vote/vote-demo.spec.js', 'specs/vote/vote-delayed.spec.js'],
 	baseUrl: 'http://localhost:8085',
 	suites: {
